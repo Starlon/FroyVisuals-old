f748cf6697f11d28f649cf9f05861d5383cfcb59
Scott Sibley <sisibley@gmail.com>
2012-01-29 14:48:59 -0600

    More Fixes.

--- jni/Android.mk
+++ jni/Android.mk
@@ -5,6 +5,7 @@ LOCAL_MODULE	:= libvisual
 LOCAL_SRC_FILES := libvisual/lv_utils.c  libvisual/lv_beat.c  libvisual/lv_cache.c  libvisual/lv_cpu.c  libvisual/lv_gl.c  libvisual/lv_libvisual.c  libvisual/lv_mem.c  libvisual/lv_palette.c  libvisual/lv_rectangle.c  libvisual/lv_time.c  libvisual/lv_video.c  libvisual/lv_bin.c  libvisual/lv_collection.c  libvisual/lv_error.c  libvisual/lv_hashlist.c  libvisual/lv_list.c  libvisual/lv_morph.c   libvisual/lv_param.c   libvisual/lv_ringbuffer.c  libvisual/lv_transform.c  libvisual/lv_video_simd.c  libvisual/lv_actor.c  libvisual/lv_bmp.c   libvisual/lv_color.c   libvisual/lv_event.c   libvisual/lv_hashmap.c  libvisual/lv_log.c  libvisual/lv_object.c  libvisual/lv_plugin.c  libvisual/lv_songinfo.c  libvisual/lv_ui.c libvisual/lv_audio.c  libvisual/lv_buffer.c  libvisual/lv_config.c  libvisual/lv_fourier.c  libvisual/lv_input.c  libvisual/lv_math.c  libvisual/lv_os.c  libvisual/lv_random.c  libvisual/lv_thread.c
 LOCAL_LDLIBS	:= -Wall
 LOCAL_CFLAGS	:= -O0 -g
+LOCAL_LDLIBS    := -lm -llog
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
--- jni/froyvisuals.c
+++ jni/froyvisuals.c
@@ -25,7 +25,7 @@
 #include <tinyalsa/asoundlib.h>
 #include <libvisual.h>
 
-#define  LOG_TAG    "libplasma"
+#define  LOG_TAG    "libfroyvisuals"
 #define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
 
@@ -41,231 +41,6 @@ static double now_ms(void)
     struct timeval tv;
     gettimeofday(&tv, NULL);
     return tv.tv_sec*1000. + tv.tv_usec/1000.;
-}
-
-/* We're going to perform computations for every pixel of the target
- * bitmap. floating-point operations are very slow on ARMv5, and not
- * too bad on ARMv7 with the exception of trigonometric functions.
- *
- * For better performance on all platforms, we're going to use fixed-point
- * arithmetic and all kinds of tricks
- */
-
-typedef int32_t  Fixed;
-
-#define  FIXED_BITS           16
-#define  FIXED_ONE            (1 << FIXED_BITS)
-#define  FIXED_AVERAGE(x,y)   (((x) + (y)) >> 1)
-
-#define  FIXED_FROM_INT(x)    ((x) << FIXED_BITS)
-#define  FIXED_TO_INT(x)      ((x) >> FIXED_BITS)
-
-#define  FIXED_FROM_FLOAT(x)  ((Fixed)((x)*FIXED_ONE))
-#define  FIXED_TO_FLOAT(x)    ((x)/(1.*FIXED_ONE))
-
-#define  FIXED_MUL(x,y)       (((int64_t)(x) * (y)) >> FIXED_BITS)
-#define  FIXED_DIV(x,y)       (((int64_t)(x) * FIXED_ONE) / (y))
-
-#define  FIXED_DIV2(x)        ((x) >> 1)
-#define  FIXED_AVERAGE(x,y)   (((x) + (y)) >> 1)
-
-#define  FIXED_FRAC(x)        ((x) & ((1 << FIXED_BITS)-1))
-#define  FIXED_TRUNC(x)       ((x) & ~((1 << FIXED_BITS)-1))
-
-#define  FIXED_FROM_INT_FLOAT(x,f)   (Fixed)((x)*(FIXED_ONE*(f)))
-
-typedef int32_t  Angle;
-
-#define  ANGLE_BITS              9
-
-#if ANGLE_BITS < 8
-#  error ANGLE_BITS must be at least 8
-#endif
-
-#define  ANGLE_2PI               (1 << ANGLE_BITS)
-#define  ANGLE_PI                (1 << (ANGLE_BITS-1))
-#define  ANGLE_PI2               (1 << (ANGLE_BITS-2))
-#define  ANGLE_PI4               (1 << (ANGLE_BITS-3))
-
-#define  ANGLE_FROM_FLOAT(x)   (Angle)((x)*ANGLE_PI/M_PI)
-#define  ANGLE_TO_FLOAT(x)     ((x)*M_PI/ANGLE_PI)
-
-#if ANGLE_BITS <= FIXED_BITS
-#  define  ANGLE_FROM_FIXED(x)     (Angle)((x) >> (FIXED_BITS - ANGLE_BITS))
-#  define  ANGLE_TO_FIXED(x)       (Fixed)((x) << (FIXED_BITS - ANGLE_BITS))
-#else
-#  define  ANGLE_FROM_FIXED(x)     (Angle)((x) << (ANGLE_BITS - FIXED_BITS))
-#  define  ANGLE_TO_FIXED(x)       (Fixed)((x) >> (ANGLE_BITS - FIXED_BITS))
-#endif
-
-static Fixed  angle_sin_tab[ANGLE_2PI+1];
-
-static void init_angles(void)
-{
-    int  nn;
-    for (nn = 0; nn < ANGLE_2PI+1; nn++) {
-        double  radians = nn*M_PI/ANGLE_PI;
-        angle_sin_tab[nn] = FIXED_FROM_FLOAT(sin(radians));
-    }
-}
-
-static __inline__ Fixed angle_sin( Angle  a )
-{
-    return angle_sin_tab[(uint32_t)a & (ANGLE_2PI-1)];
-}
-
-static __inline__ Fixed angle_cos( Angle  a )
-{
-    return angle_sin(a + ANGLE_PI2);
-}
-
-static __inline__ Fixed fixed_sin( Fixed  f )
-{
-    return angle_sin(ANGLE_FROM_FIXED(f));
-}
-
-static __inline__ Fixed  fixed_cos( Fixed  f )
-{
-    return angle_cos(ANGLE_FROM_FIXED(f));
-}
-
-/* Color palette used for rendering the plasma */
-#define  PALETTE_BITS   8
-#define  PALETTE_SIZE   (1 << PALETTE_BITS)
-
-#if PALETTE_BITS > FIXED_BITS
-#  error PALETTE_BITS must be smaller than FIXED_BITS 
-#endif
-
-static uint16_t  palette[PALETTE_SIZE];
-
-static uint16_t  make565(int red, int green, int blue)
-{
-    return (uint16_t)( ((red   << 8) & 0xf800) |
-                       ((green << 2) & 0x03e0) |
-                       ((blue  >> 3) & 0x001f) );
-}
-
-static void init_palette(void)
-{
-    int  nn, mm = 0;
-    /* fun with colors */
-    for (nn = 0; nn < PALETTE_SIZE/4; nn++) {
-        int  jj = (nn-mm)*4*255/PALETTE_SIZE;
-        palette[nn] = make565(255, jj, 255-jj);
-    }
-
-    for ( mm = nn; nn < PALETTE_SIZE/2; nn++ ) {
-        int  jj = (nn-mm)*4*255/PALETTE_SIZE;
-        palette[nn] = make565(255-jj, 255, jj);
-    }
-
-    for ( mm = nn; nn < PALETTE_SIZE*3/4; nn++ ) {
-        int  jj = (nn-mm)*4*255/PALETTE_SIZE;
-        palette[nn] = make565(0, 255-jj, 255);
-    }
-
-    for ( mm = nn; nn < PALETTE_SIZE; nn++ ) {
-        int  jj = (nn-mm)*4*255/PALETTE_SIZE;
-        palette[nn] = make565(jj, 0, 255);
-    }
-}
-
-static __inline__ uint16_t  palette_from_fixed( Fixed  x )
-{
-    if (x < 0) x = -x;
-    if (x >= FIXED_ONE) x = FIXED_ONE-1;
-    int  idx = FIXED_FRAC(x) >> (FIXED_BITS - PALETTE_BITS);
-    return palette[idx & (PALETTE_SIZE-1)];
-}
-
-/* Angles expressed as fixed point radians */
-
-static void init_tables(void)
-{
-    init_palette();
-    init_angles();
-}
-
-static void fill_plasma( AndroidBitmapInfo*  info, void*  pixels, double  t )
-{
-    Fixed ft  = FIXED_FROM_FLOAT(t/1000.);
-    Fixed yt1 = FIXED_FROM_FLOAT(t/1230.);
-    Fixed yt2 = yt1;
-    Fixed xt10 = FIXED_FROM_FLOAT(t/3000.);
-    Fixed xt20 = xt10;
-
-#define  YT1_INCR   FIXED_FROM_FLOAT(1/100.)
-#define  YT2_INCR   FIXED_FROM_FLOAT(1/163.)
-
-    int  yy;
-    for (yy = 0; yy < info->height; yy++) {
-        uint16_t*  line = (uint16_t*)pixels;
-        Fixed      base = fixed_sin(yt1) + fixed_sin(yt2);
-        Fixed      xt1 = xt10;
-        Fixed      xt2 = xt20;
-
-        yt1 += YT1_INCR;
-        yt2 += YT2_INCR;
-
-#define  XT1_INCR  FIXED_FROM_FLOAT(1/173.)
-#define  XT2_INCR  FIXED_FROM_FLOAT(1/242.)
-
-#if OPTIMIZE_WRITES
-        /* optimize memory writes by generating one aligned 32-bit store
-         * for every pair of pixels.
-         */
-        uint16_t*  line_end = line + info->width;
-
-        if (line < line_end) {
-            if (((uint32_t)line & 3) != 0) {
-                Fixed ii = base + fixed_sin(xt1) + fixed_sin(xt2);
-
-                xt1 += XT1_INCR;
-                xt2 += XT2_INCR;
-
-                line[0] = palette_from_fixed(ii >> 2);
-                line++;
-            }
-
-            while (line + 2 <= line_end) {
-                Fixed i1 = base + fixed_sin(xt1) + fixed_sin(xt2);
-                xt1 += XT1_INCR;
-                xt2 += XT2_INCR;
-
-                Fixed i2 = base + fixed_sin(xt1) + fixed_sin(xt2);
-                xt1 += XT1_INCR;
-                xt2 += XT2_INCR;
-
-                uint32_t  pixel = ((uint32_t)palette_from_fixed(i1 >> 2) << 16) |
-                                   (uint32_t)palette_from_fixed(i2 >> 2);
-
-                ((uint32_t*)line)[0] = pixel;
-                line += 2;
-            }
-
-            if (line < line_end) {
-                Fixed ii = base + fixed_sin(xt1) + fixed_sin(xt2);
-                line[0] = palette_from_fixed(ii >> 2);
-                line++;
-            }
-        }
-#else /* !OPTIMIZE_WRITES */
-        int xx;
-        for (xx = 0; xx < info->width; xx++) {
-
-            Fixed ii = base + fixed_sin(xt1) + fixed_sin(xt2);
-
-            xt1 += XT1_INCR;
-            xt2 += XT2_INCR;
-
-            line[xx] = palette_from_fixed(ii / 4);
-        }
-#endif /* !OPTIMIZE_WRITES */
-
-        // go to next line
-        pixels = (char*)pixels + info->stride;
-    }
 }
 
 /* simple stats management */
@@ -363,7 +138,7 @@ stats_endFrame( Stats*  s )
     s->lastTime = now;
 }
 
-JNIEXPORT void JNICALL Java_com_example_plasma_PlasmaView_renderPlasma(JNIEnv * env, jobject  obj, jobject bitmap,  jlong  time_ms)
+JNIEXPORT void JNICALL Java_com_starlon_froyvisuals_FroyVisualsView_renderFroyVisuals(JNIEnv * env, jobject  obj, jobject bitmap,  jlong  time_ms)
 {
     AndroidBitmapInfo  info;
     void*              pixels;
@@ -377,11 +152,8 @@ JNIEXPORT void JNICALL Java_com_example_plasma_PlasmaView_renderPlasma(JNIEnv * 
     int depth = 16;
 
     if (!init) {
-
-        init_tables();
         stats_init(&stats);
         init = 1;
-
 	    if(!visual_is_initialized())
 	    {
 	            visual_init_path_add( "/data/local/libvisual/plugins/input");
