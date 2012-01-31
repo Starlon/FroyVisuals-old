855c83394732549bc567505a01f694cdc40330c0
Scott Sibley <sisibley@gmail.com>
2012-01-30 19:43:51 -0600

    Change LOGTAG to "FroyVisuals" instead of "libfroyvisuals"

--- jni/froyvisuals.c
+++ jni/froyvisuals.c
@@ -25,7 +25,7 @@
 #include <tinyalsa/asoundlib.h>
 #include <libvisual.h>
 
-#define  LOG_TAG    "libfroyvisuals"
+#define  LOG_TAG    "FroyVisuals"
 #define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 #define  LOGW(...)  __android_log_print(ANDROID_LOG_WARN,LOG_TAG,__VA_ARGS__)
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
