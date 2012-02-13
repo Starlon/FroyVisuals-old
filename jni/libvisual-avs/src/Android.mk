0a50beaa001f23dd3281b05d7f3d1a96af26af25
Scott Sibley <sisibley@gmail.com>
2012-02-13 00:47:53 -0600

    Delete some more files.

--- jni/libvisual-avs/src/Android.mk
+++ jni/libvisual-avs/src/Android.mk
@@ -3,10 +3,10 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_C_INCLUDES := ./include
 LOCAL_MODULE	:= actor_avs
-LOCAL_SRC_FILES := $(addprefix /, $(notdir $(wildcard $(LOCAL_PATH)/*.c) $(wildcard $(LOCAL_PATH)/*.cpp)))
+LOCAL_SRC_FILES := $(addprefix /src, $(notdir $(wildcard $(LOCAL_PATH)/*.c) $(wildcard $(LOCAL_PATH)/*.cpp)))
 LOCAL_CFLAGS	:= -Wall -O0 -g
 LOCAL_LDFLAGS :=    -lgomp
-LOCAL_C_INCLUDE := common/ evaluator/
+LOCAL_C_INCLUDES := common/ evaluator/
 LOCAL_STATIC_LIBRARIES := libvisual common
 include $(BUILD_SHARED_LIBRARY)
 
