78e80a58b7d2593cfa9b87a9ee36722c18f7ecd7
Scott Sibley <sisibley@gmail.com>
2012-03-05 02:17:36 -0600

    Add members to Widget.

--- src/com/starlon/libscriptable/Widget.java
+++ src/com/starlon/libscriptable/Widget.java
@@ -2,6 +2,13 @@ package com.starlon.libscriptable;
 
 public class Widget
 {
+    protected Core mVisitor;
+    protected String mName; 
+    protected float mRow;
+    protected float mCol;
+    protected float mLayer;
+    protected int mType;
+    
     public Widget()
     {
     }
