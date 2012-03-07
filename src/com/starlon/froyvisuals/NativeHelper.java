e43f46e848587ebb72159b7817e769d294dfa059
Scott Sibley <sisibley@gmail.com>
2012-03-06 18:12:18 -0600

    Add comments to NativeHelper.

--- src/com/starlon/froyvisuals/NativeHelper.java
+++ src/com/starlon/froyvisuals/NativeHelper.java
@@ -19,6 +19,17 @@ public class NativeHelper
     public static native String inputGetAbout(int index);
     public static native String inputGetHelp(int index);
     public static native String inputGetLicense(int index);
+    public static native int inputParamGetCount();
+    public static native String inputParamGetType(int index);
+    public static native String inputParamGetName(int index);
+    public static native String inputParamGetString(String name);
+    public static native boolean inputParamSetString(String name, String newstring);
+    public static native int inputParamGetInteger(String name);
+    public static native boolean inputParamSetInteger(String name, int newint);
+    public static native float inputParamGetFloat(String name);
+    public static native boolean inputParamSetFloat(String name, float newfloat);
+    public static native double inputParamGetDouble(String name);
+    public static native boolean inputParamSetDouble(String name, double newdouble);
 
     // VisMorph native calls
     public static native int morphCount();
@@ -32,6 +43,17 @@ public class NativeHelper
     public static native String morphGetAbout(int index);
     public static native String morphGetHelp(int index);
     public static native String morphGetLicense(int index);
+    public static native int morphParamGetCount();
+    public static native String morphParamGetType(int index);
+    public static native String morphParamGetName(int index);
+    public static native String morphParamGetString(String name);
+    public static native boolean morphParamSetString(String name, String newstring);
+    public static native int morphParamGetInteger(String name);
+    public static native boolean morphParamSetInteger(String name, int newint);
+    public static native float morphParamGetFloat(String name);
+    public static native boolean morphParamSetFloat(String name, float newfloat);
+    public static native double morphParamGetDouble(String name);
+    public static native boolean morphParamSetDouble(String name, double newdouble);
 
     // VisActor native calls
     public static native int actorCount();
@@ -45,21 +67,43 @@ public class NativeHelper
     public static native String actorGetAbout(int index);
     public static native String actorGetHelp(int index);
     public static native String actorGetLicense(int index);
+    public static native int actorParamGetCount();
+    public static native String actorParamGetType(int index);
+    public static native String actorParamGetName(int index);
+    public static native String actorParamGetString(String name);
+    public static native boolean actorParamSetString(String name, String newstring);
+    public static native int actorParamGetInteger(String name);
+    public static native boolean actorParamSetInteger(String name, int newint);
+    public static native float actorParamGetFloat(String name);
+    public static native boolean actorParamSetFloat(String name, float newfloat);
+    public static native double actorParamGetDouble(String name);
+    public static native boolean actorParamSetDouble(String name, double newdouble);
 
+    /* Switch plugins */
     public static native int cycleInput(int prev);
     public static native int cycleMorph(int prev);
     public static native int cycleActor(int prev);
     public static native void finalizeSwitch(int prev);
-    public static native void updatePlugins();
     public static native void setMorphStyle(boolean style);
-    public static native boolean render(Bitmap  bitmap);
+    public static native void updatePlugins();
+
+    /* Audio input */
     public static native void resizePCM(int size, int rate, int channels, int encoding);
     public static native void uploadAudio(short[] data);
-    public static native void initApp(int w, int h, int device, int card);
+
+    /* Mouse and key events */
     public static native void mouseMotion(float x, float y);
     public static native void mouseButton(int button, float x, float y);
+
+    /* Resize vis */
     public static native void screenResize(int w, int h);
+
+    /* Init and quit libvisual */
     public static native void visualsQuit();
+    public static native void initApp(int w, int h, int device, int card);
+
+    /* Draw and render vis */
+    public static native boolean render(Bitmap  bitmap);
 
     /** constructor */
     public NativeHelper(Context c)
