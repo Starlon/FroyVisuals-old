e56d86a300ccbbfe7745cc970b11b394f4a772d8
Scott Sibley <sisibley@gmail.com>
2012-03-07 04:17:13 -0600

    Enable x86 abi. Profiling support no longer affects this lib.

--- jni/libvisual/Android.mk
+++ jni/libvisual/Android.mk
@@ -18,8 +18,9 @@ LOCAL_CFLAGS += $(OPTIM_FLAGS)
 LOCAL_LDLIBS    := -lm -ldl -llog 
 
 #profiling
-LOCAL_LDLIBS += -L$(call host-path, $(LOCAL_PATH))/$(TARGET_ARCH_ABI) -landprof
-LOCAL_CFLAGS += -pg -DVISUAL_HAVE_PROFILING -fno-omit-frame-pointer -fno-function-sections
+#LOCAL_LDLIBS += -L$(call host-path, $(LOCAL_PATH))/$(TARGET_ARCH_ABI) -landprof
+#LOCAL_CFLAGS += -pg -DVISUAL_HAVE_PROFILING -fno-omit-frame-pointer -fno-function-sections
+
 
 LOCAL_SRC_FILES := lv_utils.c  lv_beat.c  lv_cache.c  lv_cpu.c  lv_gl.c  lv_libvisual.c  lv_mem.c  lv_palette.c  lv_rectangle.c  lv_time.c  lv_video.c  lv_bin.c  lv_collection.c  lv_error.c  lv_hashlist.c  lv_list.c  lv_morph.c   lv_param.c   lv_ringbuffer.c  lv_transform.c  lv_video_simd.c  lv_actor.c  lv_bmp.c   lv_color.c   lv_event.c   lv_hashmap.c  lv_log.c  lv_object.c  lv_plugin.c  lv_songinfo.c  lv_ui.c lv_audio.c  lv_buffer.c  lv_config.c  lv_fourier.c  lv_input.c  lv_math.c  lv_os.c  lv_random.c  lv_thread.c kiss_fft129/kiss_fft.c
 
@@ -38,10 +39,14 @@ LOCAL_SHARED_LIBRARIES := cpufeatures
 
 ifeq ($(TARGET_ARCH_ABI),armeabi)
     LOCAL_CFLAGS += -DVISUAL_ARCH_ARM=1 
+    LOCAL_LDLIBS += -L$(call host-path, $(LOCAL_PATH))/$(TARGET_ARCH_ABI) -landprof
+    LOCAL_CFLAGS += -pg -DVISUAL_HAVE_PROFILING -fno-omit-frame-pointer -fno-function-sections
 endif
 
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
     LOCAL_CFLAGS += -DVISUAL_ARCH_ARM=1
+    LOCAL_LDLIBS += -L$(call host-path, $(LOCAL_PATH))/$(TARGET_ARCH_ABI) -landprof
+    LOCAL_CFLAGS += -pg -DVISUAL_HAVE_PROFILING -fno-omit-frame-pointer -fno-function-sections
 endif
 
 #LOCAL_CFLAGS += -fno-omit-frame-pointer -fno-function-sections
