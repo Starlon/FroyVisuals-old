fe8543a9b1a3e95c7531023d2c579635d153ed42
Scott Sibley <sisibley@gmail.com>
2012-03-16 19:45:38 -0500

    Turn off a few things.

--- jni/actor_avs/visscript/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_C_INCLUDES := ./include
-LOCAL_MODULE	:= visscript-arm
-LOCAL_SRC_FILES := $(addprefix /, $(notdir $(wildcard $(LOCAL_PATH)/*.c) $(wildcard $(LOCAL_PATH)/*.cpp)))
-LOCAL_CFLAGS	:= -Wall -O0 -g
-include $(BUILD_SHARED_LIBRARY)
-
