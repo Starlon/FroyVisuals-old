edfa647fd93e584c06e77e2eab7a7211f435527a
Scott Sibley <sisibley@gmail.com>
2012-03-17 00:59:33 -0500

    build liblua.a for arm and x86. place them under libs/$(targetabi)

--- jni/lua-5.1/android.sh
+++ jni/lua-5.1/android.sh
@@ -5,3 +5,5 @@ NDKVER=$NDK/toolchains/arm-linux-androideabi-4.4.3
 NDKP=$NDKVER/prebuilt/linux-x86/bin/arm-linux-androideabi-
 NDKF="--sysroot $NDK/platforms/android-$NDKABI/arch-arm"
 make linux HOST_CC="gcc -m32" CROSS=$NDKP TARGET_FLAGS="$NDKF" TARGET=arm
+#make linux HOST_CC="gcc -m32" CROSS=$NDKP TARGET_FLAGS="$NDKF" TARGET=x86
+make local
