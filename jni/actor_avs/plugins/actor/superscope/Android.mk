4a392be4f5bac65993e1638724321a756f97a39e
Scott Sibley <sisibley@gmail.com>
2012-03-17 01:59:51 -0500

    More luajit

--- jni/actor_avs/plugins/actor/superscope/Android.mk
+++ jni/actor_avs/plugins/actor/superscope/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_C_INCLUDES := \
+LOCAL_C_INCLUDES += \
     $(LOCAL_PATH)/../../../common \
     $(LOCAL_PATH)/../../../include \
     $(LOCAL_PATH)/../../../src\
@@ -9,7 +9,8 @@ LOCAL_C_INCLUDES := \
 LOCAL_MODULE	:= actor_avs_superscope
 LOCAL_SRC_FILES := actor_avs_superscope.c
 LOCAL_CFLAGS	:= -Wall -O0 -g
-LOCAL_LDLIBS += -L$(call host-path, jni/LuaJIT))/$(TARGET_ARCH_ABI) -lluajit -l -ldl
+
+LOCAL_LDLIBS += -L$(call host-path, $(LOCAL_PATH))/$(TARGET_ARCH_ABI) -lluajit -ldl
 LOCAL_SHARED_LIBRARIES := libvisual common
 LOCAL_STATIC_LIBRARIES := evaluator
 include $(BUILD_SHARED_LIBRARY)
