594cb5f6f409b34289ae845b06865dc7c1bcd22e
Scott Sibley <sisibley@gmail.com>
2012-03-31 18:40:50 -0500

    rename visscript-lua to just lua

--- StarVisuals/jni/LuaJIT/Android.mk
+++ StarVisuals/jni/LuaJIT/Android.mk
@@ -10,7 +10,7 @@ LOCAL_CFLAGS += $(OPTIM_FLAGS)
 
 LOCAL_LDLIBS += -L$(call host-path, $(LOCAL_PATH))/$(TARGET_ARCH_ABI) -llibluajit
 
-LOCAL_MODULE	:= visscript-lua
+LOCAL_MODULE	:= lua
 LOCAL_SRC_FILES := visscript-lua/visscript-lua.c
 
 LOCAL_SHARED_LIBRARIES := visual
