7a225bdcd06d4918a48e6a9079ce5a13d986b83a
Scott Sibley <sisibley@gmail.com>
2012-03-31 03:32:35 -0500

    Adjust gitigore.

--- StarVisuals/jni/LCDControl/Android.mk
+++ StarVisuals/jni/LCDControl/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_C_INCLUDES := ./include
 LOCAL_MODULE	:= LCDControl
 LOCAL_SRC_FILES := $(addprefix /, $(notdir $(wildcard $(LOCAL_PATH)/*.c) $(wildcard $(LOCAL_PATH)/*.cpp)))
-LOCAL_CFLAGS	:= -Wall -O0 -g libs/linux-gcc-4.6.1/libjson_linux-gcc-4.6.1_libmt.a
+LOCAL_CFLAGS	:= -Wall -O0 -g jsoncpp/libs/linux-gcc-4.6.1/libjson_linux-gcc-4.6.1_libmt.a
 LOCAL_SHARED_LIBRARIES := libvisual visscript-lua
 include $(BUILD_SHARED_LIBRARY)
 
