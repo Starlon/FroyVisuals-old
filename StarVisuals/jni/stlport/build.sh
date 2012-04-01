48dad6da53c31df68446767f988fd9283d1e6d9f
Scott Sibley <sisibley@gmail.com>
2012-03-31 19:43:11 -0500

    So Android has stlport since Android 2.3 :)

--- StarVisuals/jni/stlport/build.sh
+++ StarVisuals/jni/stlport/build.sh
@@ -2,5 +2,6 @@
 #!/bin/sh
 ./configure --prefix=./ --bindir=bin/ --libdir=lib/ \
     --includedir=../../include \
-    --target=android  --with-cxx=arm-none-eabi-c++ --cc=arm-none-eabi-gcc \
-    --use-compiler-family=gcc --enable-static
+    --target=android --enable-static 
+
+
