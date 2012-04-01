38005ef4a44ec9868dab501887ca4b4a1ef3f1a6
Scott Sibley <sisibley@gmail.com>
2012-04-01 01:00:27 -0500

    Add fetch.sh

--- StarVisuals/jni/actor_lcdcontrol/Android.mk
+++ StarVisuals/jni/actor_lcdcontrol/Android.mk
@@ -5,6 +5,6 @@ LOCAL_C_INCLUDES := ./include
 LOCAL_MODULE	:= LCDControl
 LOCAL_SRC_FILES := $(addprefix /, $(notdir $(wildcard $(LOCAL_PATH)/*.c) $(wildcard $(LOCAL_PATH)/*.cpp)))
 LOCAL_CFLAGS	:= -Wall -O0 -g $(LOCAL_PATH)/jsoncpp/libs/linux-gcc-4.6.1/libjson_linux-gcc-4.6.1_libmt.a
-LOCAL_SHARED_LIBRARIES := libvisual visscript-lua
+LOCAL_SHARED_LIBRARIES := libvisual lua
 #include $(BUILD_SHARED_LIBRARY)
 
---
+++ fetch.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+git fetch git@github.com:Libvisual/libvisual.git
+git merge FETCH_HEAD
