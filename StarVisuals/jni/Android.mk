52a792142b7010f84b6a2aea67895acffe353e6c
Scott Sibley <sisibley@gmail.com>
2012-04-01 06:44:03 -0500

    Polish up some Makefiles.

--- StarVisuals/jni/Android.mk
+++ StarVisuals/jni/Android.mk
@@ -1,21 +1,7 @@
 
 WARNING_FLAGS := -Wall -Wstrict-aliasing -Wcast-align -Waddress -Wmissing-braces -Wimplicit -Wunused -Wno-unused-variable
 DEBUG_FLAGS := -g -ggdb3 
-OPTIM_FLAGS := -O0 
-#-O3 -ftree-vectorize -ffast-math -funsafe-math-optimizations -fsingle-precision-constant
-ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
-    LOCAL_CFLAGS += -DNO_MMX
-    OPTIM_FLAGS += -DHAVE_ARM=1 -DHAVE_NEON=1 -mfloat-abi=softfp -mfpu=neon
-endif
-ifeq ($(TARGET_ARCH_ABI),armeabi)
-    LOCAL_CFLAGS += -DNO_MMX
-    OPTIM_FLAGS += -DHAVE_ARM=1 -mfloat-abi=softfp
-endif
-ifeq ($(TARGET_ARCH_ABI),x86)
-    # No MMX because the MMX assembly under actor_avs is for Windows.
-    LOCAL_CFLAS += -DNO_MMX
-    DEBUG_FLAGS := -g
-endif
+OPTIM_FLAGS := -O2 -ftree-vectorize -ffast-math -funsafe-math-optimizations -fsingle-precision-constant
 
 include $(call all-subdir-makefiles)
 
--- libvisual/libvisual/Android.mk
+++ libvisual/libvisual/Android.mk
@@ -14,6 +14,7 @@ LOCAL_SHARED_LIBRARIES := lua
 LOCAL_CFLAGS += $(WARNING_FLAGS)
 LOCAL_CFLAGS += $(DEBUG_FLAGS)
 LOCAL_CFLAGS += $(OPTIM_FLAGS)
+LOCAL_CLFAGS += -Iprivate/
 
 LOCAL_LDLIBS    := -lm -ldl -llog 
 
@@ -21,11 +22,17 @@ LOCAL_LDLIBS    := -lm -ldl -llog
 #LOCAL_LDLIBS += -L$(call host-path, $(LOCAL_PATH))/$(TARGET_ARCH_ABI) -landprof
 #LOCAL_CFLAGS += -pg -DVISUAL_HAVE_PROFILING -fno-omit-frame-pointer -fno-function-sections
 
-LOCAL_SRC_FILES := $(addprefix /, $(notdir $(wildcard $(LOCAL_PATH)/*.c) $(wildcard $(LOCAL_PATH)/*.cpp)))
-LOCAL_SRC_FILES += private/lv_video_convert.c  private/lv_video_fill.c  private/lv_video_scale.c
+PRIV := private/lv_video_convert.c  private/lv_video_fill.c  private/lv_video_scale.c
+
+LOCAL_SRC_FILES := $(PRIV) $(addprefix /, $(notdir $(wildcard $(LOCAL_PATH)/*.c) $(wildcard $(LOCAL_PATH)/*.cpp)))
+
+
+ifeq ($(TARGET_ARCH_ABI),armeabi)
+    LOCAL_CFLAGS += -march=armv6
+endif
 
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
-    #LOCAL_CFLAGS += -DVISUAL_ARCH_ARM=1 -DHAVE_NEON=1 -mfloat-abi=softfp -mfpu=neon
+    LOCAL_CFLAGS += -DHAVE_NEON=1 -mfloat-abi=softfp -mfpu=neon
 endif
 
 ifeq ($(TARGET_ARCH_ABI),x86)
@@ -33,18 +40,6 @@ ifeq ($(TARGET_ARCH_ABI),x86)
 endif
 
 LOCAL_SHARED_LIBRARIES := cpufeatures
-
-ifeq ($(TARGET_ARCH_ABI),armeabi)
-    #LOCAL_CFLAGS += -DVISUAL_ARCH_ARM=1 
-    #LOCAL_LDLIBS += -L$(call host-path, $(LOCAL_PATH))/$(TARGET_ARCH_ABI) -landprof
-    #LOCAL_CFLAGS += -pg -DVISUAL_HAVE_PROFILING -fno-omit-frame-pointer -fno-function-sections
-endif
-
-ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
-    #LOCAL_CFLAGS += -DVISUAL_ARCH_ARM=1
-    #LOCAL_LDLIBS += -L$(call host-path, $(LOCAL_PATH))/$(TARGET_ARCH_ABI) -landprof
-    #LOCAL_CFLAGS += -pg -DVISUAL_HAVE_PROFILING -fno-omit-frame-pointer -fno-function-sections
-endif
 
 include $(BUILD_SHARED_LIBRARY)
 $(call import-module,cpufeatures) 
