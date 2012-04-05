8f97ac09d9659151291ac837554feda57c7033ba
Scott Sibley <sisibley@gmail.com>
2012-04-05 11:51:19 -0500

    Bug fix/typo

--- StarVisuals/src/com/starlon/starvisuals/AboutPluginsActivity.java
+++ StarVisuals/src/com/starlon/starvisuals/AboutPluginsActivity.java
@@ -1,63 +1,51 @@
 package com.starlon.starvisuals;
 
-import android.content.Intent;
-import android.content.Context;
-import android.widget.ListView;
-import android.widget.ScrollView;
-import android.widget.TextView;
-import android.widget.SimpleAdapter;
-import android.widget.ArrayAdapter;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.app.Activity;
-import android.app.ListActivity;
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceFragment;
-import android.content.res.Resources;
-import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+import android.app.ListActivity;
 import android.util.Log;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.ArrayList;
-import java.util.HashMap;
+import android.content.Intent;
 
 
 public class AboutPluginsActivity extends ListActivity
 {
-        private final static String TAG = "StarVisuals/AboutPluginsActivity";
-        private final static String PREFS = "StarVisualsPrefs";
-        
-        /** called by OS when app is created initially */
-        @Override
-        public void onCreate(Bundle savedInstanceState)
-        {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.preferences);
+    private final static String TAG = "StarVisuals/AboutPluginsActivity";
 
-            final Context mContext = (Context)this;
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_plugins);
+        ListView listView = (ListView) findViewById(android.R.id.list);
 
-            ListView listView = (ListView) findViewById(android.R.id.list);
-            String[] values = new String[] { "About Plugins" };
-            final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        final LayoutInflater factory = getLayoutInflater();
 
-            listView.setAdapter(adapter);
-        
-            listView.setOnItemClickListener(new OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) 
-                {
-                    String item = (String) adapter.getItem(position);
+        String[] names = new String[] {
+            "Actors", "Inputs", "Morphs"
+        };
 
-                    if(item.equals("About Plugins"))
-                        startActivity(new Intent(mContext, AboutPluginsActivity.class));
-                    
-                }
-            });
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, names);
 
-        }
+        listView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView listView, View view, int position, long id)
+    {
+        String item = (String) listView.getAdapter().getItem(position);
+        if(item.equals("Actors"))
+                startActivity(new Intent(this, ActorPreferencesActivity.class));
+        else if(item.equals("Inputs"))
+                startActivity(new Intent(this, InputPreferencesActivity.class));
+        else if(item.equals("Morphs"))
+                startActivity(new Intent(this, MorphPreferencesActivity.class));
+    }
 }
 
